33964d7934c442510ec25c6a1cd47f0970eeb654
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-06-18 02:05:53 +0300

    Slight edit on Makka & Madina images

--- lib/Features/Home/Presentation/Views/Widgets/quran_fehres_surah_image.dart
+++ lib/Features/Home/Presentation/Views/Widgets/quran_fehres_surah_image.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_img/flutter_img.dart';
 
 class QuranFehresMaccaAndMadinaImages extends StatelessWidget
 {
-  const QuranFehresMaccaAndMadinaImages({Key? key, required this.surahType, }) : super(key: key);
+  const QuranFehresMaccaAndMadinaImages({Key? key, required this.surahType}) : super(key: key);
   final String surahType;
 
   @override
   Widget build(BuildContext context)
   {
-    return surahType == 'Makkah' ? Image.asset(
-      'assets/images/Kaaba - 3D Ramadhan Illustration Pack ( Perspective).png',
-      height: 70,
-    ) :
-    Image.asset(
-      'assets/images/Mosque - 3D Ramadhan Illustration Pack (Perspective).png',
-      height: 70,
-    )
-    ;
+    return surahType == 'Makkah' ?
+    const Img('assets/images/Kaaba - 3D Ramadhan Illustration Pack ( Perspective).png', height: 70, width: 70) :
+    const Img('assets/images/Mosque - 3D Ramadhan Illustration Pack (Perspective).png', height: 70, width: 70);
   }
 }
